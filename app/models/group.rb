56b0bffaa8d3be315ff8e0ea5f7fa701f49bda1b
suprfrye <tara.c.frye@gmail.com>
2015-04-01 11:38:31 -0400

    Update group user relationship

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-  has_many :users
+  has_and_belongs_to_many :users
   has_many :assignments
   belongs_to :creator, class_name: 'User'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :groups
+  has_and_belongs_to_many :groups
   has_many :likes
   has_many :assignments
   # has_many :users, through: :assignments
