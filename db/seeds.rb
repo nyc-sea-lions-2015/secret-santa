decd2e361a8f13d25966abb7dfd1394b77763e3d
suprfrye <tara.c.frye@gmail.com>
2015-04-01 10:59:56 -0400

    Fix typo user5

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ user1 = User.create(first_name: 'Tara', email: 'tara@gmail.com', password: '1234
 user2 = User.create(first_name: 'Ben', email: 'ben@gmail.com', password: '1234')
 user3 = User.create(first_name: 'Sally', email: 'sally@gmail.com', password: '1234')
 user4 = User.create(first_name: 'Nora', email: 'nora@gmail.com', password: '1234')
-user5 = User.create(first_name: 'Tom', email: 'tom@gmail.com,' password: '1234')
+user5 = User.create(first_name: 'Tom', email: 'tom@gmail.com', password: '1234')
 user6 = User.create(first_name: 'Mike', email: 'mike@gmail.com', password: '1234')
 
 group1 = Group.create(name: 'Sea Lions', creator: user1, spending_limit: 25.00, users: [user1, user2, user3])
