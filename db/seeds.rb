07d8ac090851c99cf26b867d681a2734d494a064
suprfrye <tara.c.frye@gmail.com>
2015-04-01 10:52:54 -0400

    Add like seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,12 @@ user6 = User.create(first_name: Mike, email: mike@gmail.com, password: '1234')
 group1 = Group.create(name: 'Sea Lions', creator: user1, spending_limit: 25.00, users: [user1, user2, user3])
 group2 = Group.create(name: 'Jackals', creator: user4, spending_limit: 35.00, users: [user4, user5, user6])
 group3 = Group.create(name: 'Phase Up', creator: user3, spending_limit: 100.00, users: [user1, user2, user3, user4, user5, user6])
+
+like1 = Like.create(user: user1, content: 'Longboarding')
+like2 = Like.create(user: user1, content: 'NY Knicks')
+like3 = Like.create(user: user2, content: 'Obey clothing')
+like4 = Like.create(user: user3, content: 'Steven King')
+like5 = Like.create(user: user4, content: 'Sushi')
+like6 = Like.create(user: user5, content: 'Kung fu Flix')
+like7 = Like.create(user: user6, content: 'white wine')
+
