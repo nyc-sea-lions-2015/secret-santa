338e9ffa9f8b0b8d0b9223a548e2a579ef7b1329
suprfrye <tara.c.frye@gmail.com>
2015-04-01 16:39:23 -0400

    Remove error conditional

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,7 +13,6 @@ put '/auth/login' do
     session[:user_id] = user.id
     redirect "/users/#{user.id}"
   else
-    @error = 'Invalid email or password.'
     redirect '/auth/login?error=noauth'
   end
 end
@@ -29,13 +28,11 @@ post '/auth/signup' do
     session[:user_id] = @user.id
     redirect "/users/<%= @user.id %>"
   else
-    @error = 'Error signing up. Please try again.'
     redirect '/auth/signup?error=noauth'
   end
 end
 
 delete '/auth/logout' do
   session[:user_id] = nil
-  @error = 'You have successfully logged out.'
   redirect '/auth/login'
 end
